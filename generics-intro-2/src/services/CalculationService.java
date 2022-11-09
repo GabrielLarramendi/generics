ef0e81c1dad57b5bd29f25acd31e63b9e5b0cd74
larramendi <larramendi.dev@gmail.com>
2022-11-09 13:44:42 -0300

    .

--- generics-intro-2/src/services/CalculationService.java
+++ generics-intro-2/src/services/CalculationService.java
@@ -4,16 +4,18 @@ import java.util.List;
 
 public class CalculationService<T> {
 
-    public static <T extends Comparable<T>>  T max(List<T> list) {
+    public static <T extends Comparable<? super T>>  T max(List<T> list) {
         if (list.isEmpty()) {
             throw new IllegalStateException("list can't be empty!");
         }
+
         T max = list.get(0);
         for (T item : list) {
             if (item.compareTo(max) > 0) {
                 max = item;
             }
         }
+
         return max;
     }
 }
